62055ff5dad272817ab507517caa644e03f08b51
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-01-30 16:53:55 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+int n,count=0;
+printf("%d",n);
+scanf("%d",&n);
+while(n!=0)
+{
+n=n/10;
+count++;
+}
+printf("no of digits:%d",count);
+return 0;
+getch();
+}
