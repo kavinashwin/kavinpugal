32e495a788899e7e3ff79fec1ebb6b4d43135830
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-01-30 17:00:20 +0530

    Create voewlnum.c

---
+++ voewlnum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+char let:
+printf("Enter the letter:",let);
+scanf("%c",&let);
+{
+if(let=='a')
+printf("%c is a vowel:,let);
+else if (let=='e')
+printf("%c is a vowel:,let);
+else if(let=='i')
+printf("%C is a vowel:,let);
+else if (let=='o')
+printf("%c is a vowel:,let);
+else if(let=='u')
+printf("%c is a vowel",let);
+else 
+printf("%c is a constant",let);
+}
+getch();
+}
