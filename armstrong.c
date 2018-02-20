37521608ec4b3ffc80ef1eb255e2d2c7922c824f
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-02-20 16:37:10 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,l,r,reverse=0,c=0,cube; 
+scanf("%d",&n); 
+l=n;
+while(n!=0) 
+{ 
+r=n%10; 
+cube=r*r*r;
+reverse=reverse+cube;
+n=n/10;
+ } 
+ if(reverse==l) 
+ printf("yes"); 
+ else printf("no");
+ return 0;
+ getch();
+ }
