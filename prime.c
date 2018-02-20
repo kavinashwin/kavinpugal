dff039bdd81a1c3e843c8d69b61407c16aada214
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-02-20 15:59:51 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,flag=0,i;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<n/2;i++)
+{
+if(n%2==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("prime");
+}
+else
+{
+printf("not prime");
+}
+getch();
+}
