76e709481ab28132bb1f7212400e42f8cbcb6405
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-02 15:04:05 +0530

    Create kavin34.c

---
+++ kavin34.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+{
+int main(void) {
+	char a[50];
+	int i,count=1;
+	gets(a);
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i] == '.')
+		count++;
+		
+		
+	}
+	printf("%d",count);
+	return 0;
+getch();
+}
