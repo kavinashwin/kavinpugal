cf7d4e410c20f3dcd09ed6e1d1be8c9925e93573
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-02 14:54:09 +0530

    Create kavin32.c

---
+++ kavin32.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch[100];
+	int i,k,count=1;
+	scanf("%[^\n]s",ch);
+	for(k=0;ch[k]!='\0';++k)
+	{
+		
+	}
+	for(i=0;i<k;i++)
+	{
+	
+	if(ch[i]==' '||ch[i]=='/n'||ch[i]=='\t')
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+	return 0;
+getch();
+}
