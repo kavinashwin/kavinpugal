b0d57af9fabfe7350927a2f3145928cc134c09f1
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-03 16:29:34 +0530

    Create kavin48.c

---
+++ kavin48.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(void) {
+int n,arr[10],sum=0,avg;
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+{
+scanf("%d\n",&arr[i]);
+}
+for(int i=0;i<n;i++)
+{
+sum+=arr[i];
+}
+avg=sum/n;
+printf("%d",avg);
+return 0;
+}
