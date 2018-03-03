27f488632e3711904bb8ab794ceb001bc87b521d
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-03 15:50:57 +0530

    Create kavin41.c

---
+++ kavin41.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int main()
+{
+ int i=5;
+ char s[10]={'g','u','v','i'};
+ for(i=0;i<=5;i++)
+ {
+     printf("%s \n",s);
+ }
+     
+ getch();
+ }
