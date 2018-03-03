d25bc46976a92041e158175a2b863aaef11d095c
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-03 16:35:36 +0530

    Create kavin50.c

---
+++ kavin50.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+  void main()
+            {
+            	int n=10;
+            	if(n%2==0)
+            	{
+     
+            	printf("yes");
+     
+            	}
+            	else
+            	{
+            		printf("no");
+            	}
+            getch();
+            }
