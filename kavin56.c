67261d555b03233fc1a1c4abf914f9b525ad4a46
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-13 16:30:35 +0530

    Create kavin56.c

---
+++ kavin56.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+char str;
+printf("enter the string");
+gtes(str);
+if((str>a)&&(str<z)||(str>0)&&(str<9))
+{
+printf("it contains alphabet and numbers");
+}
+else
+{
+printf("no numbers and alphabets");
+}
+getch();
+}
