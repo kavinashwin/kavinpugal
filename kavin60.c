9cd512a5dcbfb59d81e5ea64cfa8923f4dfb3605
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-15 16:03:47 +0530

    Create kavin60.c

---
+++ kavin60.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+scanf("%d",&n);
+int a[n];
+a[0]=1;
+a[1]=1;
+for(i=2;i<n;i++)
+{
+  a[i]=a[i-2]+a[i-1] ;
+    
+}
+for(i=0;i<n;i++)
+{
+printf("%d ",a[i]);
+}
+getch();
+}
