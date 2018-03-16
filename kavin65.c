2882cd57d3ad15824d9a6971343fc59e0d62dc51
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-16 14:10:46 +0530

    Create kavin65.c

---
+++ kavin65.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h
+int main()
+{
+
+	char str[50];
+	int i;
+	printf("enter the string:");
+	scanf("%[^\n]s",str);
+	if(printf("\n%s",str))
+	{
+		
+	}
+	return 0;
+getch();
+}
