9f57ccc8caa86a36b0ba3b8cba7f0e2a6084b02c
kavinashwin <35653683+kavinashwin@users.noreply.github.com>
2018-03-16 14:30:36 +0530

    Create kavin66.c

---
+++ kavin66.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+
+printf("enter a number");
+scanf("%d",n);
+if(n%2!=0)
+{
+printf("prime number");
+}
+else
+{
+printf("not a prime number");
+}
+getch();
+}
