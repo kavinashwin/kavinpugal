f4a1fa721bc14d766eeedd8eb1c214e53151da9c
kavinpugal <35653683+kavinpugal@users.noreply.github.com>
2018-03-20 16:21:49 +0530

    Create kavin73.c

---
+++ kavin73.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,l,r;
+printf("\n Enter n,l,r:");
+scanf("%d %d %d",&n,&l,&r);
+if(n>=l&&n<=r)
+{
+printf("\nYES");
+}
+else
+{
+printf("\nNO");
+}
+return 0;
+grtch();
+}
